da4af812686a60236c9c945af6d8960ab6d5099d
Linh Truong <tvlinh@orientsoftware.net>
2016-07-29 08:00:49 +0700

    Init Domain layer

---
+++ SimpleWebApi.Domain/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleWebApi.Domain.Entities
+{
+    public class Category : IEntityBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+        public virtual ICollection<Gadget> Gadgets { get; set; }
+
+        public Category()
+        {
+            DateCreated = DateTime.Now;
+        }
+    }
+}
